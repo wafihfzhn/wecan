8f0a1d8caf89f4b1833fb663637c480a1055180b
Muslih <muslihzarth@gmail.com>
2020-03-02 05:04:27 +0700

    Implement routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router } from "@reach/router";
+import Route from "./components/Route";
+import Home from "./components/pages/Home";
+import Sum from "./components/pages/Sum";
+import Multiply from "./components/pages/Multiply";
+import Prime from "./components/pages/Prime";
+import Fibonacci from "./components/pages/Fibo";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Route path="/" pageComponent={<Home />} />
+    <Route path="/sum" pageComponent={<Sum />} />
+    <Route path="/multiply" pageComponent={<Multiply />} />
+    <Route path="/prime" pageComponent={<Prime />} />
+    <Route path="/fibonacci" pageComponent={<Fibonacci />} />
+  </Router>
+);
 
 export default App;
