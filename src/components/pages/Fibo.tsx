1d0c518d54e21f37325005f58f7183b69234bce4
Muslih <muslihzarth@gmail.com>
2020-03-02 05:07:34 +0700

    Add fibo page

---
+++ src/components/pages/Fibo.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import fibonacci from "../../utils/fibonacci";
+import "./Home.scss";
+
+const Prime = () => {
+  const [result, setResult] = React.useState<number[]>([0]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    const currentTarget = event.currentTarget;
+    const nVal: number = +currentTarget["primeFactor"].value || 0;
+
+    setResult(fibonacci(nVal));
+  };
+
+  React.useEffect(() => {
+    setResult([0]);
+  }, []);
+
+  const SumForm = () => {
+    return (
+      <div>
+        <form className="sumform" onSubmit={handleSubmit}>
+          <input
+            type="number"
+            name="primeFactor"
+            placeholder="First N prime number"
+            autoFocus
+          />
+          <button type="submit">Calculate</button>
+        </form>
+      </div>
+    );
+  };
+
+  return (
+    <div className="Sum">
+      <h1>Fibo</h1>
+      <p>
+        {" "}
+        In this page, you will find first N prime number, and print the result
+      </p>
+      <SumForm />
+      Result:{" "}
+      {result.map((r: number, i: number) => (
+        <div className="result" key={i} style={{ display: "inline" }}>
+          {i ? ", " : ""}
+          {r}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Prime;
---
+++ src/utils/fibonacci.ts
@@ -0,0 +1,18 @@
+const fibonacciSequence = (n: number) => {
+  const result = [];
+
+  let firstNum = 0,
+    secNum = 1;
+
+  for (var i = 0; i <= n; i++) {
+    const temp = firstNum;
+    firstNum = secNum;
+    secNum = temp + secNum;
+
+    result.push(temp);
+  }
+
+  return result;
+};
+
+export default fibonacciSequence;
