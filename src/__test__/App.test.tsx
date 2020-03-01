c3a812cbdd83f89aa59d11545852278a7e22170f
Muslih <muslihzarth@gmail.com>
2020-03-02 05:51:55 +0700

    Update packages

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import Adapter from "enzyme-adapter-react-15";
 import { render } from "@testing-library/react";
+import { mount, configure } from "enzyme";
 import App from "../App";
+import { navigate } from "@reach/router";
+import Home from "../components/pages/Home";
 
+configure({ adapter: new Adapter() });
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Wecan/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+describe("Renders correct routes", (): void => {
+  describe("home page", (): void => {
+    const wrapper = mount(<App />);
+    navigate("/");
+
+    console.log(wrapper);
+  });
 });
