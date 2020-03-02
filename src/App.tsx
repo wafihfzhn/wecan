27e86889e85512d4cc571441539b3992e6710351
Muslih <muslihzarth@gmail.com>
2020-03-02 14:01:21 +0700

    Update app component

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ import Sum from "./components/pages/Sum";
 import Multiply from "./components/pages/Multiply";
 import Prime from "./components/pages/Prime";
 import Fibonacci from "./components/pages/Fibo";
-import "./App.css";
+import "./App.scss";
 
 const App = () => (
-  <div>
+  <div className="App">
     <h1>Wecan</h1>
     <Router>
       <Route path="/" pageComponent={<Home />} />
