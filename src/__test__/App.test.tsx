e8f4ef0b78f012290db6dd70149dc3036051c053
Muslih <muslihzarth@gmail.com>
2020-03-02 11:10:05 +0700

    Add test for sumpage

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,23 +1,30 @@
 import React from "react";
-import Adapter from "enzyme-adapter-react-15";
-import { render } from "@testing-library/react";
+import Adapter from "enzyme-adapter-react-16";
 import { mount, configure } from "enzyme";
 import App from "../App";
-import { navigate } from "@reach/router";
 import Home from "../components/pages/Home";
+import Sum from "../components/pages/Sum";
+import { navigate } from "@reach/router";
 
 configure({ adapter: new Adapter() });
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Wecan/i);
-  expect(linkElement).toBeInTheDocument();
-});
 
 describe("Renders correct routes", (): void => {
   describe("home page", (): void => {
-    const wrapper = mount(<App />);
-    navigate("/");
+    it("should render correct component", () => {
+      navigate("/");
+      const wrapper = mount(<App />);
 
-    console.log(wrapper);
+      wrapper.update();
+      expect(wrapper.find(Home)).toHaveLength(1);
+    });
+  });
+  describe("sum page", (): void => {
+    it("should render correct component", () => {
+      navigate("/sum");
+      const wrapper = mount(<App />);
+
+      wrapper.update();
+      expect(wrapper.find(Sum)).toHaveLength(1);
+    });
   });
 });
