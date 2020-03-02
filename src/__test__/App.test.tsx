66608a4e61b456bf3a7ff7ff2f9382c7cd5eb7bd
Muslih <muslihzarth@gmail.com>
2020-03-02 11:37:14 +0700

    Add test Apap page

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -4,27 +4,52 @@ import { mount, configure } from "enzyme";
 import App from "../App";
 import Home from "../components/pages/Home";
 import Sum from "../components/pages/Sum";
+import Multiply from "../components/pages/Sum";
+import Prime from "../components/pages/Prime";
+import Fibonacci from "../components/pages/Fibo";
 import { navigate } from "@reach/router";
 
 configure({ adapter: new Adapter() });
 
 describe("Renders correct routes", (): void => {
+  const wrapper = mount(<App />);
   describe("home page", (): void => {
-    it("should render correct component", () => {
-      navigate("/");
-      const wrapper = mount(<App />);
+    it("should render correct component", async (): Promise<void> => {
+      await navigate("/");
 
       wrapper.update();
       expect(wrapper.find(Home)).toHaveLength(1);
     });
   });
   describe("sum page", (): void => {
-    it("should render correct component", () => {
-      navigate("/sum");
-      const wrapper = mount(<App />);
+    it("should render correct component", async (): Promise<void> => {
+      await navigate("/sum");
 
       wrapper.update();
       expect(wrapper.find(Sum)).toHaveLength(1);
+    });
+  });
+  describe("multiply page", (): void => {
+    it("should render correct component", async (): Promise<void> => {
+      await navigate("/multiply");
+
+      expect(wrapper.find(Multiply)).toHaveLength(1);
+    });
+  });
+  describe("prime page", (): void => {
+    it("should render correct component", async (): Promise<void> => {
+      await navigate("/prime");
+
+      wrapper.update();
+      expect(wrapper.find(Prime)).toHaveLength(1);
+    });
+  });
+  describe("fibonacci page", (): void => {
+    it("should render correct component", async (): Promise<void> => {
+      await navigate("/fibonacci");
+
+      wrapper.update();
+      expect(wrapper.find(Fibonacci)).toHaveLength(1);
     });
   });
 });
