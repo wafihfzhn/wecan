136a9b8c36c807b6025cc65abce2c9520c795414
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-02 16:33:33 +0000

    Deploy muslih/wecan to github.com/muslih/wecan.git:gh-pages

---
+++ precache-manifest.e63b5caacc90f8f2918f9589c76ba110.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119da1c3f1d6f11afe5d151c3acf9072",
+    "url": "/wecan/index.html"
+  },
+  {
+    "revision": "f0e2171dc7cdbfcdbfd7",
+    "url": "/wecan/static/css/main.8f4d9286.chunk.css"
+  },
+  {
+    "revision": "830be9fa840eca813d65",
+    "url": "/wecan/static/js/2.f2715579.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/wecan/static/js/2.f2715579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0e2171dc7cdbfcdbfd7",
+    "url": "/wecan/static/js/main.73265fec.chunk.js"
+  },
+  {
+    "revision": "3b234a8f5143f52f8f82",
+    "url": "/wecan/static/js/runtime-main.0fa0c88a.js"
+  }
+]);
